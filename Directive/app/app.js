8ead9991078d8918d338e399954440d9fc6a4a5d
unknown <debadyuti.karmakar@gmail.com>
2022-01-19 16:26:50 +0530

    ng-click

--- Directive/app/app.js
+++ Directive/app/app.js
@@ -2,35 +2,44 @@ let myapp = angular.module('myapp',[]);
 
 myapp.controller('myappcontroller',['$scope', function($scope){
 
+    $scope.removedata = function(institutes){
+        let removedata = $scope.institute.indexOf(institutes);
+        $scope.institute.splice(removedata,1);
+    }
     $scope.institute =[ 
         {
             name:"Debadyuti",
             course:"Node Js",
             fees:100,
+            
             available: true
         },
         {
             name:"Ayan",
             course:"Java",
             fees:80,
+            
             available: true
         },
         {
             name:"Saheb",
             course:"Python",
             fees:56,
+    
             available: false
         },
         {
             name:"Parthib",
             course:"C",
             fees:40,
+            
             available: true
         },
         {
             name:"Rajdeep",
             course:"C++",
             fees:60,
+            
             available: true
         }
 
